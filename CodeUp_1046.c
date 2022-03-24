5282dede115befaca045448b7192c601babe2b79
gimjh003 <46261152+gimjh003@users.noreply.github.com>
2022-03-25 02:12:24 +0900

    2022-03-25

---
+++ CodeUp_1046.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+int main(){
+    int a, b, c;
+    scanf("%d %d %d", &a, &b, &c);
+    long long int sum = a+b+c;
+    printf("%lld\n", sum);
+    printf("%.1f", (float)sum/3);
+    return 0;
+}
